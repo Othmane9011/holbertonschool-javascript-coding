313e5da89184fc216f13db68d73f588e98122f22
othmane9011 <5650@holbertonstudents.com>
2023-10-13 12:00:53 +0200

    removal

--- javascript-web_scraping/5-request_store.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const fs = require('fs');
-const url = process.argv[2];
-const fileName = process.argv[3];
-
-const fileStream = fs.createWriteStream(fileName);
-request(url).pipe(fileStream);
